c0d1e2f10c6af052780281600915a2c71b0e954a
Hillel Dror <58593238+hillelbu65@users.noreply.github.com>
2022-07-01 18:32:58 +0300

    Config the react-router mechanism to create single page application mchanisem and added tow main pages: login and month overview

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Main from './components/Main';
 
 function App() {
   return (
     <div className="App">
-        <h1>Ok let's go!! :)</h1>
+        <Main/>
     </div>
   );
 }
