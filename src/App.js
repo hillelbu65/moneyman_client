14f0b25fc5f9e216cd50ed85cb0e8a1bdf4d0ce3
Hillel Dror <58593238+hillelbu65@users.noreply.github.com>
2022-07-15 17:20:17 +0300

    Upadte the app component

--- src/App.js
+++ src/App.js
@@ -3,10 +3,9 @@ import React, { useContext, useEffect, useState } from 'react'
 import './App.css';
 import { gapi } from 'gapi-script'
 import Main from './components/Main';
-import { getMonth } from './components/month_overview_page/Data_management/pullDatat';
 
-const API_KEY = 'IzaSyBSZFasZYRXVWT_hezt0FPlW7AAM3ZL3pI'
-const CLIENT_ID = '214100381972-lpdih25hg9rhkk8tb37lsbmev4aflr7d.apps.googleusercontent.com';
+const API_KEY = '<YOUR API KEY>'
+const CLIENT_ID = '<YOUR CLIENT ID>';
 const SCOPES = 'https://www.googleapis.com/auth/drive'
 
 function App() {
@@ -23,15 +22,9 @@ function App() {
     gapi.load('client:auth2', start);
   }, []);
 
-  const test = async () => {
-    const resolt = await getMonth('1oHvBvgpsMOUHSDVUocOtGLjJyRj6p6H_krfkSX2UgUs', 'test', '03', '2022')
-    console.log(resolt)
-  }
-
   return (
     <div className="App">
       <Main/>
-      <button onClick={test}>Test Data</button>
     </div>
   );
 }
